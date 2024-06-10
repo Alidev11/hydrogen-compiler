67bf52d26b8627a6d9bc443722954d488f0c79de
Alidev11 <karwawiali@gmail.com>
2024-06-10 22:43:53 +0100

    simple compiler without parse tree

--- hydrogen/src/main.cpp
+++ hydrogen/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <sstream> // Include for std::stringstream
 #include <fstream>
+#include <sstream> // Include for std::stringstream
 #include <vector>
 #include <optional>
 
@@ -14,15 +14,72 @@ enum class TokenType{
 
 struct Token{
     TokenType type;
-    optional<string> value;
+    optional<string> value {};
 };
 
 vector<Token> tokenize(const string& str){
-    for(char c : str){
-        cout << c << endl;
+    vector<Token> tokens;
+    string buf;
+
+    for(int i = 0; i< str.length(); i++){
+        char c = str.at(i);
+        if(isalpha(c)){
+            buf.push_back(c);
+            i++;
+            while(isalnum(str.at(i))){
+                buf.push_back(str.at(i));
+                i++;
+            }
+            i--;
+
+            if(buf == "return"){
+                tokens.push_back({.type = TokenType::_return});
+                buf.clear();
+                continue;
+            }else{
+                cerr << "Tou messed up!" << endl;
+                exit(EXIT_FAILURE);
+            }
+        } else if(isdigit(c)){
+            buf.push_back(c);
+            i++;
+            while(isdigit(str.at(i))){
+                buf.push_back(str.at(i));
+                i++;
+            }
+            i--;
+            tokens.push_back({.type = TokenType::int_lit, .value = buf});
+            buf.clear();
+        }else if(c == ';'){
+            tokens.push_back({.type = TokenType::semi});
+        }else if(isspace(c)){
+            continue; 
+        }else{
+            cerr << "You messed up!" << endl;
+            exit(EXIT_FAILURE); 
+        }
     }
+    return tokens;
 }
 
+string tokens_to_asm(vector<Token>& tokens){
+    stringstream output;
+    output << "global _start \n_start:\n";
+
+    for(int i = 0; i < tokens.size(); i++){
+        const Token& token = tokens.at(i);
+        if(token.type == TokenType::_return){
+             if(i+1 < tokens.size() && tokens.at(i+1).type == TokenType::int_lit){
+                if(i+2 < tokens.size() && tokens.at(i+2).type == TokenType::semi){
+                    output << "     mov rax, 60\n";
+                    output << "     mov rdi, " << tokens.at(i+1).value.value() << "\n";
+                    output << "     syscall";
+                }
+             }
+        }
+    }
+    return output.str();
+}
 
 int main(int argc, char* argv[]){
     if(argc != 2){
@@ -41,6 +98,14 @@ int main(int argc, char* argv[]){
     }
 
 
-    vector<Token> t = tokenize(contents);
+    vector<Token> tokens = tokenize(contents);
+
+    {
+        fstream file("../out.asm", ios::out);
+        file << tokens_to_asm(tokens);
+    }
+
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
     return EXIT_FAILURE;
 }
